c3181b1ed4982723e4db07e77c8e79075c5b822a
benniao_chen <benniao_chen@163.com>
2018-10-30 23:51:47 +0800

    change package.json && add express server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const baseDir = __dirname + '/';
+const angularDir = baseDir + '/angular-basic-frame/dist/angular-basic-frame/';
+
+app.use(express.static(angularDir));
+
+app.use('/', function(request, response, next) {
+	response.sendFile(angularDir + 'index.html');
+});
+
+app.listen(3000, function() {
+    console.log('server start 3000');
+})
